3579d09b06f44ec0a5259374c426097c0a205869
Matheus <matheusssleite@outlook.com>
2020-11-25 18:34:49 -0300

    SECOND COMMIT OF DAY

--- PalacePetz/Scripts/Limpar.js
+++ PalacePetz/Scripts/Limpar.js
@@ -1,9 +1,11 @@
 ﻿var senha = document.getElementById("senha");
 var limpar = document.getElementById("limpar");
 
+function Entrar() {
 
+}
 function Senha() {
-    document.getElementById("limpar").style.visibility = "visible";
+    document.getElementById("limpar").style.visibility = "visible";   
 }
 function Limpar() {
     document.getElementById("senha").focus();
